857984476e7adef62b3400b886b2a0b630b0d04f
Alex Olsen <alexolsen@Alexs-MacBook-Pro.local>
2017-08-26 22:07:26 -0500

    better contact page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,18 @@ function windowH() {
     $('.main-bg').css({height: wH});
     $('.main').css({height: wH});
     $('.face-shot').css({height: wH});
+
+    var contactHeight = $('.form-container').css('min-height');
+    var contactRight = $('.form-container').css('right');
+
+    $('.contact-main-bg').css({height: contactHeight});
+    $('.korea-photo').css({height: contactHeight});
+    $('.img-form-container').css({height: contactHeight});
+}
+
+function onContact() {
+  console.log('on contact...');
+  $(".contact").get(0).scrollIntoView();
 }
 
 windowH();
