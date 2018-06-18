3c6079e19ba4ff02675548677c3a5767a22f1492
Alex Olsen <alexolsen@Alexs-MacBook-Pro.local>
2018-06-17 20:51:11 -0500

    send ze form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,78 @@
 'use strict';
 
+function loading(el) {
+  $(el).attr('class', 'btn btn-primary');
+  $(el).html('<i class="fa fa-spin fa-spinner">');
+  $(el).prop('disabled', true);
+}
+
+function doneLoading(el) {
+
+  $(el).attr('class', 'btn btn-success');
+  $(el).html('<i class="fa fa-check"></i> Sent');
+  $(el).prop('disabled', false);
+}
+
+function sendEmail(body, btnId) {
+  loading(btnId);
+
+  var email = {
+    subject: "Jleetranslations Work Request",
+    body: body
+  };
+
+  $.ajax({
+    type: "POST",
+    url: "https://rna9ms5pn0.execute-api.us-east-1.amazonaws.com/production/submit",
+    crossDomain: true,
+    data: JSON.stringify(email),
+    contentType: "application/json",
+    dataType: "json",
+    success: function(data, status) {
+      doneLoading(btnId);
+    }
+  });
+}
+
+function requestMsg() {
+  var name = $('#msgName').val();
+  var email = $('#msgEmail').val();
+  var body = $('#msgBody').val();
+
+  if (!name || !email || !body) {
+    alert('Please enter a name, email and body text');
+    return;
+  }
+
+  var emailBody = `
+    Name:  ${name} \n
+    Email: ${email} \n \n
+
+    Message: \n
+    ${body}
+  `;
+
+  sendEmail(emailBody, '#msgBtn');
+}
+
+function requestCall() {
+  var name = $('#callName').val();
+  var phone = $('#callPhone').val();
+
+  if (!name || !phone) {
+    alert('Please enter a name and a phone number');
+    return;
+  }
+
+  var emailBody = `
+    Name:  ${name} \n \n
+
+    ${name} is requesting a call at ${phone}
+  `;
+
+  sendEmail(emailBody, '#callBtn');
+}
+
 function isBreakpoint( alias ) {
   return $('.device-' + alias).is(':visible');
 }
@@ -12,18 +85,13 @@ function windowH() {
     $('.face-shot').css({height: wH});
 
     if ( !isBreakpoint('xs') ) {
-      var contactHeight = $('.form-container').css('min-height');
-      $('.contact-main-bg').css({height: contactHeight});
-      $('.korea-photo').css({height: contactHeight});
-      $('.img-form-container').css({height: contactHeight});
+      // var contactHeight = $('.form-container').css('min-height');
+      // $('.korea-photo').css({height: contactHeight});
     } else {
       var mH = '700px';
       $('.main-bg').css({height: mH});
       $('.main').css({height: mH});
       $('.face-shot').css({height: mH});
-
-      $('.contact-main-bg').css({height: mH});
-      $('.form-container').css({width: '100%'});
     }
 }
 
@@ -33,6 +101,14 @@ function onContact() {
 
 $(window).resize(function () {
   windowH();
+});
+
+$('#msgForm').submit(function(e) {
+  e.preventDefault();
+});
+
+$('#callForm').submit(function(e) {
+  e.preventDefault();
 });
 
 windowH();
